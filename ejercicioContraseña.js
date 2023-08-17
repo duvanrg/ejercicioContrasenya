d6ea4768395a66c6986cd510898ad0d883502ea4
Duban R <135860225+duvanrg@users.noreply.github.com>
2023-08-17 11:52:16 -0500

    Update ejercicioContraseña.js en clase

--- ejercicioContraseña.js
+++ ejercicioContraseña.js
@@ -2,7 +2,6 @@
 /*        ejercicio1         */
 /*---------------------------*/
 
-
 const prompt = require("prompt-sync")();                    //llamamos la funcion de prompt
 /** metodo normal
  * A = String(prompt("hackerrank")).toLowerCase();          //ingreso de la primera palabra    
@@ -10,22 +9,26 @@ const prompt = require("prompt-sync")();                    //llamamos la funcio
 */
 
 //metodo Promtp, funciona directamente desde la terminal
-A = String(prompt("primera palabra: ")).toLowerCase();      //pedimos al usuario que ingrese la primera palabra    
-B = String(prompt("segunda palabra: ")).toLowerCase();      //pedimos al usuario que ingrese la segunda palabra    
-password = "";                                              //variable vacia donde se va a guardar la contraseña
+let A = String(prompt("primera palabra: ")).toLowerCase();      //pedimos al usuario que ingrese la primera palabra    
+let B = String(prompt("segunda palabra: ")).toLowerCase();      //pedimos al usuario que ingrese la segunda palabra    
+let password = "";                                              //variable vacia donde se va a guardar la contraseña
 
-for (let i = 0; i <= (A.length + B.length); i++) {          //ciclo que recorre cada una de las palabras 
-    if (A[i] === undefined && B[i] === undefined) {         //cuando ya se acabe las posibles combinaciones se sale del ciclo
-        break
-    }
-    if (A[i] === undefined && B[i] !== undefined) {         //cuando B tenga letras disponibles pero A no
-        password += B[i]
-    }
-    if (B[i] === undefined && A[i] !== undefined) {         //cuando A tenga letras disponibles pero B no
-        password += A[i]
-    }
-    if (A[i] != undefined && B[i] != undefined) {           //cuando ambas palabras tengan letras disponibles
-        password += A[i] + B[i];
+let letras = /^[a-z]+$/;
+
+if (letras.test(A) && letras.test(B)) {
+    for (let i = 0; i <= (A.length + B.length); i++) {          //ciclo que recorre cada una de las palabras 
+        if (A[i] === undefined && B[i] === undefined) {         //cuando ya se acabe las posibles combinaciones se sale del ciclo
+            break
+        }
+        if (A[i] === undefined && B[i] !== undefined) {         //cuando B tenga letras disponibles pero A no
+            password += B[i]
+        }
+        if (B[i] === undefined && A[i] !== undefined) {         //cuando A tenga letras disponibles pero B no
+            password += A[i]
+        }
+        if (A[i] != undefined && B[i] != undefined) {           //cuando ambas palabras tengan letras disponibles
+            password += A[i] + B[i];
+        }
     }
 }
 
